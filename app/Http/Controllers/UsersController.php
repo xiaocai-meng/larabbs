8756ff57a1ac0afdb6395915cfec32aea96be83e
xiaocai-meng <1026251951@qq.com>
2018-03-30 13:03:01 +0800

    登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -32,6 +33,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
         //print_r($user);exit;
+        //让一个已认证通过的用户实例进行登录
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
