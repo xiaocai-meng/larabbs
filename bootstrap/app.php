f2f854c804eb4580a5e268af4a1ee411a693f091
xiaocai-meng <1026251951@qq.com>
2018-03-30 10:35:15 +0800

    helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+include __DIR__.'/helpers.php';
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
