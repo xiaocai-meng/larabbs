05b27fb84093fc45d609537036ee9290244bf179
xiaocai-meng <1026251951@qq.com>
2018-04-15 18:19:30 +0800

    Add password resets & email configuration

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,9 +25,16 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
-            // 登录成功后的相关操作
-            session()->flash('success', '欢迎回来！');
-            return redirect()->intended(route('users.show', [Auth::user()]));
+            if(Auth::user()->activated) {
+                // 登录成功后的相关操作
+                session()->flash('success', '欢迎回来！');
+                //该方法可将页面重定向到上一次请求尝试访问的页面上，并接收一个默认跳转地址参数，当上一次请求记录为空时，跳转到默认地址上
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
         } else {
             // 登录失败后的相关操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt('secret'),
         'is_admin' => false,
+        'activated' => true,
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
