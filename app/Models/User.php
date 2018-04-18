54bd927396e539500b89134f3b23f0ec6796d020
xiaocai-meng <1026251951@qq.com>
2018-04-18 11:38:39 +0800

    增加env配置文件

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,16 @@ class User extends Authenticatable
     {
         $this->notify(new ResetPassword($token));
     }
+
+    //一个用户拥有多条微博
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
 }
